e13b375ede222fcf565230bbc7895ac06ae3f482
thestratmanblues <icerinkman@gmail.com>
2025-11-11 11:05:34 -0500

    Updated for Python 3 compatibility

--- helix_dump.py
+++ helix_dump.py
@@ -16,6 +16,7 @@
 #
 # Version 1.0	2015/11/04	Initial release
 # Version 1.1 	2015/11/05	Add input/output file params
+# Version 1.2   2025/11/11  Updated for Python 3 compatibility
 
 import sys
 import getopt
@@ -26,50 +27,50 @@ import base64
 import zlib
 
 def main(argv):
-	inputfile = ''
-	outputfile = ''
-	data = None
-	compress_data = None
-	data_bundle = None
-	app_name = sys.argv[0]
+    inputfile = ''
+    outputfile = ''
+    data = None
+    compress_data = None
+    data_bundle = None
+    app_name = sys.argv[0]
 
-	try:
-		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-	except getopt.GetoptError:
-		print  app_name,' -i <inputfile> -o <outputfile>'
-	  	sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print(app_name, '-i <inputfile> -o <outputfile>')
+        sys.exit(2)
 
-	for opt, arg in opts:
-		if opt == '-h':
-		 	print app_name,' -i <inputfile> -o <outputfile>'
-	 		sys.exit()
-		elif opt in ("-i", "--ifile"):
-	 		inputfile = arg
-		elif opt in ("-o", "--ofile"):
-	 		outputfile = arg
-	
-	if outputfile is '':
-		outputfile = inputfile + '_JSON_dump.out'
-	
-	print ' Input file is:', inputfile
-	print 'Output file is:', outputfile
+    for opt, arg in opts:
+        if opt == '-h':
+             print(app_name,' -i <inputfile> -o <outputfile>')
+             sys.exit()
+        elif opt in ("-i", "--ifile"):
+             inputfile = arg
+        elif opt in ("-o", "--ofile"):
+             outputfile = arg
+    
+    if outputfile is '':
+        outputfile = inputfile + '_JSON_dump.out'
+    
+    print(' Input file is:', inputfile)
+    print('Output file is:', outputfile)
 
-	with open(inputfile) as file_bundle:
-		data = json.load(file_bundle)
+    with open(inputfile) as file_bundle:
+        data = json.load(file_bundle)
 
-	if 'encoded_data' in data:
-		compress_data = base64.b64decode(data['encoded_data'])
-		bundle = zlib.decompress(compress_data)
+    if 'encoded_data' in data:
+        compress_data = base64.b64decode(data['encoded_data'])
+        bundle = zlib.decompress(compress_data)
 
-		data_bundle = json.loads(bundle)
-		data_bundle_str = yaml.safe_load(bundle)
+        data_bundle = json.loads(bundle)
+        data_bundle_str = yaml.safe_load(bundle)
 
-		# yaml instead of json to remove the unicode 'u' char in output only
-		with open(outputfile, "w") as file_out:
-			pprint(data_bundle_str, file_out)   	
-				#	pprint(data_bundle, file_out)
+        # yaml instead of json to remove the unicode 'u' char in output only
+        with open(outputfile, "w") as file_out:
+            pprint(data_bundle_str, file_out)   	
+                #	pprint(data_bundle, file_out)
 
-	print 'Helix dump completed.'
+    print('Helix dump completed.')
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
